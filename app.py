4f411c4a62aefcd7cc2ff5fad240190a6f672ad3
Karim <karim.talbi7@gmail.com>
2025-11-29 22:02:26 +0100

    Initialize database tables on app startup. Remove explicit port binding for consistency.

--- app.py
+++ app.py
@@ -426,4 +426,8 @@ def invalid_movie_title(e) -> (
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
